8a2c9a9d178e61f89b24c511fb5e6ba1429fdb3d
Nikita Teterin <nekitonum@gmail.com>
2022-05-16 00:53:20 +0300

    remove redundant enum

--- socket/inc/urls.h
+++ socket/inc/urls.h
@@ -11,16 +11,6 @@
 #define RESPONSE_FILE ".response.tmp"
 #define URLS_FILE ".urls.tmp"
 
-enum {
-    URL_WAIT,
-    SOL,
-    EOL,
-    LINKS_NULL_PTR_ERROR,
-    LINKS_OK,
-    NON_COMPILED = 0,
-    COMPILED
-};
-
 void parse();
 size_t list_urls();
 const char *const pick_next_url(const size_t url_num);
